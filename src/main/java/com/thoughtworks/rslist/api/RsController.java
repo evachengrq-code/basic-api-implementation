f6e372e0ba10176425e315afe06b878c0e3b1ff6
程若前 <evachengrq@hodowakamaes-MacBook-Air.local>
2020-09-19 15:53:51 +0800

    [evacheng]feat: implemented add rs event

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -2,12 +2,21 @@ package com.thoughtworks.rslist.api;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<String> rsList = initRsList();
+
+  private List<String> initRsList() {
+    List<String> tempRsList = new ArrayList<>();
+    tempRsList.add("第一条事件");
+    tempRsList.add("第二条事件");
+    tempRsList.add("第三条事件");
+    return tempRsList;
+  }
 
   @GetMapping("/rs/list")
   public String getAllRsEvent(@RequestParam(required = false) Integer start,
@@ -22,5 +31,11 @@ public class RsController {
   //@PathVariable 表明了这个index是要加到path里的
   public String getRsEvent(@PathVariable int index) {
     return rsList.get(index - 1);
+  }
+
+  @PostMapping("/rs/event")
+  //@RequestBody 表明了要来这里拿这个body
+  public void addRsEvent(@RequestBody String rsEventString) {
+    rsList.add(rsEventString);
   }
 }
--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -6,16 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.*;;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+//在每次跑测试前回到起始状态
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class RsControllerTest {
 
     @Autowired
@@ -48,5 +52,17 @@ class RsControllerTest {
         mockMvc.perform(get("/rs/list?start=1&end=3"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"));
+    }
+
+    @Test
+    void should_add_a_rs_event() throws Exception {
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"));
+        mockMvc.perform((post("/rs/event").content("第四条事件")))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件, 第四条事件]"));
     }
 }
